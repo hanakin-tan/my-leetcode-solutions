5f3c4a1eb228609b3024472a6991db2fe1361e37
hanakin-tan <hanakin.tan1219@gmail.com>
2019-01-11 16:01:03 +0900

    add TODOs

---
+++ 101_isSymmetric.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/symmetric-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        # TODO 
---
+++ 509_fib.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+    # TODO
